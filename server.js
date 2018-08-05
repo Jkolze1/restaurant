9aa1739e2115639b90d259b6288b61d467c98768
Astronaut Group <19156046+alexanderjbeal@users.noreply.github.com>
2018-08-05 14:20:45 -0500

    Waitlist Fixed

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
-var restaurant = [
+const restaurant = [
   {
     routeName: "alex",
     name: "Alex",
@@ -46,39 +46,21 @@ app.get("/api/restaurant", function(req, res) {
   return res.json(restaurant);
 });
 
-// Displays a single character, or returns false
-app.get("/api/restaurant/:reservation", function(req, res) {
-  var chosen = req.params.reservation;
-
-  console.log(chosen);
-
-  for (var i = 0; i < restaurant.length; i++) {
-    if (chosen === restaurant[i].routeName) {
-      return res.json(restaurant[i]);
-    }
-  }
-  return res.json(false);
-});
-
-// Create New Characters - takes in JSON input
 app.post("/api/restaurant", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body-parser middleware
   var newreservation = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
   newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
-
   console.log(newreservation);
-
   restaurant.push(newreservation);
-
   res.json(newreservation);
 });
 
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+
+// if (restaurant.length >= 4) {
+//   restaurant.splice(0,4);
+//   let newWait = newreservation.splice(0,4);
+//   waitlist.push(newWait);
+//  }
